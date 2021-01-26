92a8af4321cec1817cef5bc1420f624257a6af7a
EC2 Default User <ec2-user@ip-172-31-4-68.ap-northeast-2.compute.internal>
2021-01-26 09:35:29 +0000

    check with ec2

--- Server/app.js
+++ Server/app.js
@@ -8,10 +8,10 @@ var app = express();
 
 //DB 생성
 var connection = mysql.createConnection({
-    host: "",
-    user: "",
-    database: "",
-    password: "",
+    host: "localhost",
+    user: "root",
+    database: "example",
+    password: "Capstone0!",
     port: 3306
 });
 //Middlewares
@@ -19,12 +19,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //라우팅
+app.get('/', function(req, res) {
+	res.send("Hello world!");
+	connection.query('select * from Users', function(err, rows, fields) {
+		if (err) throw err;
+		console.log('users rows : ', rows);
+	});
+	connection.end();
+});
+
 app.post('/user/join', function (req, res) {
     console.log(req.body);    
 });
 
 app.post('/user/login', function (req, res) {
-    console.log(req.body)
+    console.log(req.body);
 });
 
 //Server listening
