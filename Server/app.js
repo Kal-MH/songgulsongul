5c178146c117bf7d98a6a348dd6c3efd5f8cb7a1
Kal-MH <rkfalgus713@gmail.com>
2021-01-27 13:23:59 +0900

    basicServer - join test with localDB (temporary work)

--- Server/app.js
+++ Server/app.js
@@ -29,7 +29,29 @@ app.get('/', function(req, res) {
 });
 
 app.post('/user/join', function (req, res) {
-    console.log(req.body);    
+        var userEmail = req.body.userEmail;
+        var userPwd = req.body.userPwd;
+        var userName = req.body.userName;
+
+        // 삽입을 수행하는 sql문.
+        var sql = 'INSERT INTO users (UserEmail, UserPwd, UserName) VALUES (?, ?, ?)';
+        var params = [userEmail, userPwd, userName];
+        connection.query(sql, params, function (err, result) {
+            var resultCode = 404;
+            var message = '에러가 발생했습니다';
+    
+            if (err) {
+                console.log(err);
+            } else {
+                resultCode = 200;
+                message = '회원가입에 성공했습니다.';
+            }
+    
+            res.json({
+                'code': resultCode,
+                'message': message
+            });
+        });
 });
 
 app.post('/user/login', function (req, res) {
