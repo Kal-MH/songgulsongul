795eea684dda187ebdead9c37a2c806bb026f7c4
arami98 <arami98@naver.com>
2021-02-06 18:45:57 +0900

    android_login_modify

--- Android/paper/app/src/main/java/smu/capstone/paper/MainActivity.java
+++ Android/paper/app/src/main/java/smu/capstone/paper/MainActivity.java
@@ -9,11 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    Button temp_join;
     Button temp_login;
     Button temp_home;
-    Button temp_find;
     Button temp_upload;
     Button temp_realm;
 
@@ -23,10 +20,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        temp_join = findViewById(R.id.temp_join);
         temp_login = findViewById(R.id.temp_login);
         temp_home = findViewById(R.id.temp_home);
-        temp_find = findViewById(R.id.temp_find);
         temp_upload = findViewById(R.id.temp_upload);
         temp_realm = findViewById(R.id.go_temp_realm);
 
@@ -40,28 +35,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        temp_join.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, JoinActivity.class);
-                startActivity(intent);
-
-            }
-        });
-
 
         temp_home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        temp_find.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, FindAccountActivity.class);
                 startActivity(intent);
             }
         });
