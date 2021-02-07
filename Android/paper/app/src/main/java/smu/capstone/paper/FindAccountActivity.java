a2f09a0a0ce31a76f9dbe79d4930764650961ea3
arami98 <arami98@naver.com>
2021-02-07 22:02:27 +0900

    android_find_modify

--- Android/paper/app/src/main/java/smu/capstone/paper/FindAccountActivity.java
+++ Android/paper/app/src/main/java/smu/capstone/paper/FindAccountActivity.java
@@ -3,6 +3,7 @@ package smu.capstone.paper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
@@ -10,6 +11,7 @@ import androidx.fragment.app.FragmentTransaction;
 public class FindAccountActivity extends AppCompatActivity {
 
     Button find_id_btn, find_pw_btn;
+    ImageButton find_back_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,15 @@ public class FindAccountActivity extends AppCompatActivity {
 
         find_id_btn = (Button)findViewById(R.id.find_id_btn);
         find_pw_btn = (Button)findViewById(R.id.find_pw_btn);
+        find_back_btn = (ImageButton)findViewById(R.id.find_back_btn);
+
+
+        find_back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               onBackPressed();
+            }
+        });
 
         find_id_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +49,10 @@ public class FindAccountActivity extends AppCompatActivity {
                 transaction.commit();
             }
         });
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        FragFindId fragFindId = new FragFindId();
+        transaction.replace(R.id.find_frame, fragFindId);
+        transaction.commit();
     }
 }
