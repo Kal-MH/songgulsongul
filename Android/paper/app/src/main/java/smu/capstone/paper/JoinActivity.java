80d1ae84ad6790c72958e2e6ee711917f74e7486
arami98 <arami98@naver.com>
2021-02-09 23:44:23 +0900

    android_join_modify
    ing...

--- Android/paper/app/src/main/java/smu/capstone/paper/JoinActivity.java
+++ Android/paper/app/src/main/java/smu/capstone/paper/JoinActivity.java
@@ -1,14 +1,38 @@
 package smu.capstone.paper;
 
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class JoinActivity extends AppCompatActivity {
+
+    TextView join_timer;
+    CountDownTimer countDownTimer;
+
+    final int MILLISINFUTURE = 180 * 1000;
+    final int COUNT_DOWN_INTERVAL = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
     }
+
+    public void countDownTimer(){
+        join_timer = findViewById(R.id.join_timer);
+        countDownTimer = new CountDownTimer(MILLISINFUTURE, COUNT_DOWN_INTERVAL ) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+    }
+
 }
