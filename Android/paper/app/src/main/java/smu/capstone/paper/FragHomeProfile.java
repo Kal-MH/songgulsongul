5ab6894ff86806c9c99f1b95b0bb6b0a69bfbe03
nawonhee <nwh0921@naver.com>
2021-02-22 10:45:00 +0900

    Profile set data

--- Android/paper/app/src/main/java/smu/capstone/paper/FragHomeProfile.java
+++ Android/paper/app/src/main/java/smu/capstone/paper/FragHomeProfile.java
@@ -16,12 +16,16 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import org.w3c.dom.Text;
 
 import smu.capstone.paper.data.FeedData;
 import smu.capstone.paper.data.ProfileData;
@@ -29,11 +33,28 @@ import smu.capstone.paper.data.ProfileData;
 public class FragHomeProfile extends Fragment {
     ImageButton button;
     View view;
+    TextView userId;
+    ImageView profile_image;
+    TextView feed_counter;
+    TextView follow_counter;
+    TextView follower_counter;
+    TextView intro;
+    TextView url;
+    TextView point;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag_home_profile, container, false);
+
+        userId=(TextView)view.findViewById(R.id.profile_id);
+        profile_image = (ImageView)view.findViewById(R.id.profile_userimage);
+        feed_counter = (TextView)view.findViewById(R.id.profile_feed_cnt);
+        follow_counter = (TextView)view.findViewById(R.id.profile_follow_cnt);
+        follower_counter = (TextView)view.findViewById(R.id.profile_follower_cnt);
+        intro=(TextView)view.findViewById(R.id.profile_intro);
+        url=(TextView)view.findViewById(R.id.profile_snsurl);
+        point=(TextView)view.findViewById(R.id.profile_point);
 
         button = (ImageButton)view.findViewById(R.id.profile_setting_btn);
         button.setOnClickListener(new View.OnClickListener() {
@@ -71,16 +92,22 @@ public class FragHomeProfile extends Fragment {
         GridView gridView = view.findViewById(R.id.profile_grid);
 
         //아이템 추가
+        userId.setText("wonhee");
+        profile_image.setImageResource(R.drawable.test);
+        feed_counter.setText("20");
+        follow_counter.setText("204");
+        follower_counter.setText("204");
+        intro.setText("hello");
+        url.setText("twitter.com");
+        point.setText("4000p");
+
+
         int[] i = {R.drawable.sampleimg, R.drawable.sampleimg, R.drawable.sampleimg, R.drawable.sampleimg,
                 R.drawable.sampleimg, R.drawable.sampleimg, R.drawable.sampleimg, R.drawable.sampleimg,
                 R.drawable.sampleimg, R.drawable.sampleimg, R.drawable.sampleimg, R.drawable.sampleimg,
                 R.drawable.sampleimg, R.drawable.sampleimg, R.drawable.sampleimg, R.drawable.sampleimg,
                 R.drawable.sampleimg, R.drawable.sampleimg, R.drawable.sampleimg, R.drawable.sampleimg
         };
-
-        ProfileData data = new ProfileData("wonhee",drawable2Bitmap(getResources().getDrawable(R.drawable.sampleimg)),
-                20,204,
-                204,"hello", "twitter.com",4000);
 
         // 어뎁터 적용
         PostImageAdapter adapter = new PostImageAdapter(this.getContext(), R.layout.post_image_item, i);
