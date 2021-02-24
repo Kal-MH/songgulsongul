a934a245a1ac2b9faca9fc424a35993472987c11
yujin_Lee <sofod12@naver.com>
2021-02-24 14:45:08 +0900

    Add RoundImageView

--- Android/paper/app/src/main/java/smu/capstone/paper/activity/ProfileActivity.java
+++ Android/paper/app/src/main/java/smu/capstone/paper/activity/ProfileActivity.java
@@ -1,13 +1,21 @@
 package smu.capstone.paper.activity;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import smu.capstone.paper.R;
 
 
 public class ProfileActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
