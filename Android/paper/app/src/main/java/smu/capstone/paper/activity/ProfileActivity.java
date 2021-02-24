7e37e760dab054bb3dc74f8aa2dfdfe23b80190d
yujin_Lee <sofod12@naver.com>
2021-02-24 23:55:43 +0900

    Add Follow Adaptor and set Adaptor Modify Fragment's name Modify detail of layout

--- Android/paper/app/src/main/java/smu/capstone/paper/activity/ProfileActivity.java
+++ Android/paper/app/src/main/java/smu/capstone/paper/activity/ProfileActivity.java
@@ -3,22 +3,39 @@ package smu.capstone.paper.activity;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import smu.capstone.paper.R;
 
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private LinearLayout profile_follows;
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        profile_follows = findViewById(R.id.profile_follows);
+
+        profile_follows.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( ProfileActivity.this, FollowActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
