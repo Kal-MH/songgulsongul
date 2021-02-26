0b160db6562291867ec011f9c8a6d027aa2de657
yujin_Lee <sofod12@naver.com>
2021-02-26 09:14:52 +0900

    ProfileActivity --> EditProfileActivity 화면전환연결

---
+++ Android/paper/app/src/main/java/smu/capstone/paper/activity/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package smu.capstone.paper.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import smu.capstone.paper.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
