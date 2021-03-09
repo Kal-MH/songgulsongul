798d312e168e8526dcf5d24a287fdb7e2bb301c8
yujin_Lee <sofod12@naver.com>
2021-03-09 22:47:14 +0900

    - set Image to zoom layout

--- Android/paper/app/src/main/java/smu/capstone/paper/activity/EditActivity.java
+++ Android/paper/app/src/main/java/smu/capstone/paper/activity/EditActivity.java
@@ -2,90 +2,55 @@ package smu.capstone.paper.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-import java.io.File;
-import java.io.IOException;
+import com.bumptech.glide.Glide;
 
 import smu.capstone.paper.R;
+import smu.capstone.paper.layout.ZoomView;
 
 
 // 카메라 촬영및 갤러리에서 선택후 임시로 전달할 Activity
 public class EditActivity extends AppCompatActivity {
+    ImageView zoom_background;
+    String filePath;
+    FrameLayout zoomFrame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
 
-        ImageView imageView = findViewById(R.id.edit_img);
-
         //gets the file path
-        String filePath=getIntent().getStringExtra("path");
+       filePath = getIntent().getStringExtra("path");
 
-        //loads the file
-        File file = new File(filePath);
-        Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+        //zoom View 세팅
+        LayoutInflater inflater = this.getLayoutInflater();
+        View view = inflater.inflate(R.layout.zoom_item, null, false);
 
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        ZoomView zoomView = new ZoomView((Context)this);
+        zoomView.addView(view);
+        zoomView.setLayoutParams((ViewGroup.LayoutParams)layoutParams);
+        zoomView.setMaxZoom(4.0F);
+        zoomFrame = findViewById(R.id.edit_zoom) ;
+        zoomFrame.addView(zoomView);
+        zoom_background = findViewById(R.id.zoom_background);
 
-        //비트맵 회전
-        ExifInterface exif = null;
-        try {
-            exif = new ExifInterface(filePath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-        int exifDegree = exifOrientationToDegrees(exifOrientation);
-        bitmap = rotate(bitmap, exifDegree);
-
-
-        imageView.setImageBitmap(bitmap);
     }
 
 
-    public int exifOrientationToDegrees(int exifOrientation) {
-        if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_90)
-        {
-            return 90;
-        }
-        else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_180)
-        {
-            return 180;
-        }
-        else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_270)
-        {
-            return 270;
-        }
-        return 0;
-    }
-    public Bitmap rotate(Bitmap bitmap, int degrees){
-        if(degrees != 0 && bitmap != null)
-        {
-            Matrix m = new Matrix();
-            m.setRotate(degrees, (float) bitmap.getWidth() / 2,
-                    (float) bitmap.getHeight() / 2);
-
-            try
-            {
-                Bitmap converted = Bitmap.createBitmap(bitmap, 0, 0,
-                        bitmap.getWidth(), bitmap.getHeight(), m, true);
-                if(bitmap != converted)
-                {
-                    bitmap.recycle();
-                    bitmap = converted;
-                }
-            }
-            catch(OutOfMemoryError ex)
-            {
-                // 메모리가 부족하여 회전을 시키지 못할 경우 그냥 원본을 반환합니다.
-            }
-        }
-        return bitmap;
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        //이미지 배경화면세팅
+        Glide.with(this).load(filePath).into(zoom_background);
+        super.onWindowFocusChanged(hasFocus);
     }
 }
