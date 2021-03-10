1183ba8a7167543c2709ef2a3e01945fce485132
arami98 <arami98@naver.com>
2021-03-10 13:31:36 +0900

    AddKeepDetail
    add KeepActivity detail
    + add Feed, Comu, PostSearch detail

--- Android/paper/app/src/main/java/smu/capstone/paper/activity/KeepActivity.java
+++ Android/paper/app/src/main/java/smu/capstone/paper/activity/KeepActivity.java
@@ -9,8 +9,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -20,8 +27,9 @@ import smu.capstone.paper.item.PostItem;
 
 public class KeepActivity extends AppCompatActivity {
 
-    ArrayList<PostItem> items = new ArrayList<PostItem>();
-    TextView keep_count;
+    PostImageAdapter adapter;
+    TextView keep_count, keep_id;
+    ImageView keep_imae;
 
 
     @Override
@@ -30,6 +38,8 @@ public class KeepActivity extends AppCompatActivity {
         setContentView(R.layout.activity_keep);
 
         keep_count = findViewById(R.id.keep_count);
+        keep_id = findViewById(R.id.keep_id);
+        keep_imae = findViewById(R.id.keep_imae);
         Toolbar toolbar = (Toolbar) findViewById(R.id.keep_toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
@@ -40,24 +50,101 @@ public class KeepActivity extends AppCompatActivity {
 
         // view에서 id 찾아야함
         GridView gridView = findViewById(R.id.keep_grid);
-        //아이템 추가
-        items.add(new PostItem(R.drawable.sampleimg));
-        items.add(new PostItem(R.drawable.test));
-        items.add(new PostItem(R.drawable.ic_baseline_emoji_emotions_24));
-        items.add(new PostItem(R.drawable.test));
-        items.add(new PostItem(R.drawable.sampleimg));
-        items.add(new PostItem(R.drawable.ic_favorite));
-        items.add(new PostItem(R.drawable.sampleimg));
+
+        final JSONObject obj = getKeepData();
 
         // 어뎁터 적용
-        PostImageAdapter adapter = new PostImageAdapter(this,  R.layout.post_image_item , items ) ;
+        try {
+            adapter = new PostImageAdapter(this, R.layout.post_image_item, obj);
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
         gridView.setAdapter(adapter);
 
-        keep_count.setText("보관한 게시글 " + items.size());
+        // PostActivity에서 전달받은 프로필 사진으로 셋팅
+        //Intent intent = getIntent();
+        //keep_imae.setImageResource(intent.getIntExtra("profileImg", 0));
 
+        // 로그인한 userId로 셋팅
+        //keep_id.setText(userId);
 
+        // 보관한 게시글 개수 셋팅
+        try {
+            keep_count.setText("보관한 게시글 " + obj.getJSONArray("data").length());
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
 
+        //Click Listener
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                Intent intent = new Intent(KeepActivity.this, PostActivity.class);
+
+                // 게시글 id 전달
+                try {
+                    int postId = obj.getJSONArray("data").getJSONObject(position).getInt("postId");
+                    intent.putExtra("postId", postId);
+                } catch (JSONException e){
+                    e.printStackTrace();
+                }
+
+                startActivity(intent);
+
+                Log.d("TAG", position + "is Clicked");      // Can not getting this method.
+            }
+        });
+
+    }
+
+    //server에서 data전달
+    public JSONObject getKeepData(){
+        JSONObject item = new JSONObject();
+        JSONArray arr= new JSONArray();
+
+        //임시 데이터 저장
+        try{
+            JSONObject obj1 = new JSONObject();
+            obj1.put("postImage", R.drawable.sampleimg);
+            obj1.put("postId", 1);
+            arr.put(obj1);
+
+            JSONObject obj2 = new JSONObject();
+            obj2.put("postImage", R.drawable.test);
+            obj2.put("postId", 2);
+            arr.put(obj2);
+
+            JSONObject obj3 = new JSONObject();
+            obj3.put("postImage", R.drawable.ic_baseline_emoji_emotions_24);
+            obj3.put("postId", 3);
+            arr.put(obj3);
+
+            JSONObject obj4 = new JSONObject();
+            obj4.put("postImage", R.drawable.test);
+            obj4.put("postId", 4);
+            arr.put(obj4);
+
+            JSONObject obj5 = new JSONObject();
+            obj5.put("postImage", R.drawable.sampleimg);
+            obj5.put("postId", 5);
+            arr.put(obj5);
+
+            JSONObject obj6 = new JSONObject();
+            obj6.put("postImage", R.drawable.ic_favorite);
+            obj6.put("postId", 6);
+            arr.put(obj6);
+
+            JSONObject obj7 = new JSONObject();
+            obj7.put("postImage", R.drawable.sampleimg);
+            obj7.put("postId", 7);
+            arr.put(obj7);
+
+            item.put("data", arr);
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        return item;
     }
 
     @Override
@@ -70,5 +157,4 @@ public class KeepActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
