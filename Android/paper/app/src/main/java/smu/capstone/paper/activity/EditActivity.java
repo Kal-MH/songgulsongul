a3b76dacdcf689c3116eaf9efcbdbec48c1a0580
yujin_Lee <sofod12@naver.com>
2021-03-11 22:26:56 +0900

    점 4개 화면에 그리기 + zoom in/out

--- Android/paper/app/src/main/java/smu/capstone/paper/activity/EditActivity.java
+++ Android/paper/app/src/main/java/smu/capstone/paper/activity/EditActivity.java
@@ -3,6 +3,9 @@ package smu.capstone.paper.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +16,10 @@ import android.widget.LinearLayout;
 
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
+
 import smu.capstone.paper.R;
+import smu.capstone.paper.layout.DrawView;
 import smu.capstone.paper.layout.ZoomView;
 
 
@@ -22,6 +28,8 @@ public class EditActivity extends AppCompatActivity {
     ImageView zoom_background;
     String filePath;
     FrameLayout zoomFrame;
+    FrameLayout drawFrame;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,7 @@ public class EditActivity extends AppCompatActivity {
 
         //gets the file path
        filePath = getIntent().getStringExtra("path");
+
 
         //zoom View 세팅
         LayoutInflater inflater = this.getLayoutInflater();
@@ -40,9 +49,14 @@ public class EditActivity extends AppCompatActivity {
         zoomView.addView(view);
         zoomView.setLayoutParams((ViewGroup.LayoutParams)layoutParams);
         zoomView.setMaxZoom(4.0F);
+
         zoomFrame = findViewById(R.id.edit_zoom) ;
         zoomFrame.addView(zoomView);
         zoom_background = findViewById(R.id.zoom_background);
+
+
+
+        drawFrame = findViewById(R.id.canvas);
 
     }
 
@@ -51,6 +65,22 @@ public class EditActivity extends AppCompatActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         //이미지 배경화면세팅
         Glide.with(this).load(filePath).into(zoom_background);
+
+        Rect rect = new Rect(
+                0, 0,
+                drawFrame.getMeasuredWidth(), drawFrame.getMeasuredHeight()
+        );
+
+        ArrayList<int[]> arrayList= new ArrayList<>();
+        arrayList.add(new int[]{300, 300});
+        arrayList.add(new int[]{300, 600});
+        arrayList.add(new int[]{600, 300});
+        arrayList.add(new int[]{600, 600});
+
+        DrawView drawView = new DrawView(this,rect,arrayList);
+        drawFrame.addView(drawView);
+
+
         super.onWindowFocusChanged(hasFocus);
     }
 }
