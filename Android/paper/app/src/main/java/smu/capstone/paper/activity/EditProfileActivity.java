39ed3241e721cb9e6568de8f98ded35a589a70d2
nawonhee <nwh0921@naver.com>
2021-03-17 00:55:22 +0900

    Profile image edit

--- Android/paper/app/src/main/java/smu/capstone/paper/activity/EditProfileActivity.java
+++ Android/paper/app/src/main/java/smu/capstone/paper/activity/EditProfileActivity.java
@@ -3,21 +3,34 @@ package smu.capstone.paper.activity;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
 
+import java.io.InputStream;
+
 import smu.capstone.paper.R;
+import smu.capstone.paper.fragment.FragFindId;
 
 public class EditProfileActivity extends AppCompatActivity {
 
     RadioGroup sns_radio;
     EditText profile_new_sns;
+    Button profile_img_chnage;
+    ImageView profile_set_img;
+    private static final int REQUEST_CODE = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +44,18 @@ public class EditProfileActivity extends AppCompatActivity {
         actionBar.setTitle("프로필 수정");
         actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 만들기
         actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_new_24); //뒤로가기 버튼 이미지 지정
+
+        profile_img_chnage = findViewById(R.id.profile_img_chnage);
+        profile_set_img = findViewById(R.id.profile_set_img);
+        profile_img_chnage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 1);
+            }
+        });
 
 
         profile_new_sns = findViewById(R.id.profile_new_sns);
@@ -56,6 +81,27 @@ public class EditProfileActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                try {
+                    // 선택한 이미지에서 비트맵 생성
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    // 이미지 표시
+                    profile_set_img.setImageBitmap(img);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
