a7ed5bcfec946bdb1f49c52bf26c3492306c3910
Kal-MH <rkfalgus713@gmail.com>
2021-04-14 15:27:39 +0900

    login & daily point method

--- Server/Controller/apiController_sub.js
+++ Server/Controller/apiController_sub.js
@@ -1,41 +1,36 @@
 const statusCode = require("../config/serverStatusCode");
 const connection = require("../db/db");
 
-function generateCurrentDate() {
+function generateCurrentDate(type) {
     var date = new Date();
     var yearMonthDate = date.getFullYear() + "-" + ((date.getMonth() + 1 ) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1)) + "-" + date.getDate();
     console.log(yearMonthDate);
     return (yearMonthDate);
 }
 
-const apiController_subFunc = {
-    generateCurrentDate : function generateCurrentDate() {
-        var date = new Date();
-        var yearMonthDate = date.getFullYear() + "-" + ((date.getMonth() + 1 ) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1)) + "-" + date.getDate();
-        console.log(yearMonthDate);
-        return (yearMonthDate);
-    },
-    updatePoint : function updatePoint(req, res, sql, type, userId) {
-        connection.query(sql, function (err, result) {
-            if (err){
-                console.log(err);
+function updatePoint(req, res, sql, type, userId) {
+    connection.query(sql, function (err, result) {
+        if (err){
+            console.log(err);
+            res.json({
+                'code' : statusCode.SERVER_ERROR
+            })
+        } else {
+            if (type == 0){
                 res.json({
-                    'code' : statusCode.SERVER_ERROR
-                })
+                    'code' : statusCode.OK,
+                    'id' : userId
+                })    
             } else {
-                if (type == 0){
-                    res.json({
-                        'code' : statusCode.OK,
-                        'id' : userId
-                    })    
-                } else {
-                    res.json({
-                        'code' : statusCode.OK
-                    })
-                }
+                res.json({
+                    'code' : statusCode.OK
+                })
             }
-        })
-    },
+        }
+    })
+}
+
+const apiController_subFunc = {
     checkLastLogin : function (req, res, userId, type) {
         var selectUserSql = `select * from user where id = ${userId};`;
 
@@ -46,13 +41,14 @@ const apiController_subFunc = {
                     'code' : statusCode.CLIENT_ERROR
                 })
             } else {
-                const yearMonthDate = generateCurrentDate();
-                
-                var setLastLoginUpdatePointSql = `update user set last_login = ${yearMonthDate} where id = ${userId};`;
-                if (result[0].last_login != yearMonthDate){
+                const lastYearMonthDate = generateCurrentDate();
+
+                var setLastLoginUpdatePointSql = `update user set last_login = NOW() where id = ${userId};`;
+                if (result[0].last_login != lastYearMonthDate){
                     setLastLoginUpdatePointSql += `update user set point = point + 20 where id = ${userId};`
                 }
-
+                
+                console.log(setLastLoginUpdatePointSql);
                 updatePoint(req, res, setLastLoginUpdatePointSql, type, userId);
             }
         })
