b1cf6c72a4da55bd5fe740f1c7f7ee6cccb1caba
yujin_Lee <sofod12@naver.com>
2021-05-08 21:33:52 +0900

    Modified StatusCode.java (Client Err 204 -> 400)

--- Android/paper/app/src/main/java/smu/capstone/paper/server/StatusCode.java
+++ Android/paper/app/src/main/java/smu/capstone/paper/server/StatusCode.java
@@ -2,8 +2,8 @@ package smu.capstone.paper.server;
 
 public class  StatusCode {
     public static int RESULT_OK = 200;
-    public static int RESULT_CLIENT_ERR= 204;
     public static int RESULT_NO = 201;
+    public static int RESULT_CLIENT_ERR= 400;
     public static int RESULT_SERVER_ERR = 500;
 
     public static String getName(int a){
