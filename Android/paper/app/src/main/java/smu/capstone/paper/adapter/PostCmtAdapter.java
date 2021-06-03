bca156647f85cc947558ccc2de76d8574ebd99d4
yujin_Lee <sofod12@naver.com>
2021-06-03 22:36:14 +0900

    - Change PostActivity's UI - Change adapter to post's comments

--- Android/paper/app/src/main/java/smu/capstone/paper/adapter/PostCmtAdapter.java
+++ Android/paper/app/src/main/java/smu/capstone/paper/adapter/PostCmtAdapter.java
@@ -1,61 +1,108 @@
 package smu.capstone.paper.adapter;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
+import android.text.style.AlignmentSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
+import smu.capstone.paper.LoginSharedPreference;
 import smu.capstone.paper.R;
 import smu.capstone.paper.activity.PostActivity;
 import smu.capstone.paper.activity.ProfileActivity;
 import smu.capstone.paper.responseData.Comment;
+import smu.capstone.paper.server.RetrofitClient;
 
-public class PostCmtAdapter extends BaseAdapter {
+public class PostCmtAdapter extends RecyclerView.Adapter<PostCmtAdapter.ViewHolder>  {
 
-    ViewHolder holder;
-    Context context;
+    final Context context;
     List<Comment> dataList;
     int cmtCnt;
 
 
-    public PostCmtAdapter (Context context, List<Comment>  obj)  {
+    private OnItemLongClickEventListener mItemLongClickListener;
+
+    Date today = new Date();
+    SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+    Calendar rightNow = Calendar.getInstance();
+
+    public interface OnItemLongClickEventListener {
+        void onItemLongClick(View a_view, int a_position);
+    }
+    public void setOnItemLongClickListener(OnItemLongClickEventListener a_listener) {
+        mItemLongClickListener = a_listener;
+    }
+
+    public PostCmtAdapter (Context context, List<Comment>  obj )  {
         this.context = context;
         dataList = obj;
         cmtCnt = dataList.size();
     }
-    @Override public int getCount() {
+
+    @Override public long getItemId(int position) {
+        return dataList.get(position).getId();
+    }
+
+    @Override
+    public int getItemCount() {
         return dataList.size();
     }
-    @Override public View getView(int position, View convertView, ViewGroup parent) {
 
-        int pos = position;
-        final Context context = parent.getContext(); // "listview_item" Layout을 inflate하여 convertView 참조 획득.
 
-        if (convertView == null) {
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.post_cmt_item, parent, false);
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = inflater.inflate(R.layout.post_cmt_item, parent, false);
+        return new PostCmtAdapter.ViewHolder(itemView,mItemLongClickListener);
+    }
 
-            holder = new ViewHolder(convertView);
-
-            // 해당 View에 setTag로 Holder 객체 저장
-            convertView.setTag(holder);
-        } // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
-        else {
-            // ConvertView가 이미 생성된적 있다면, 저장되어있는 Holder 가져오기
-            holder = (ViewHolder) convertView.getTag();
-        }
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onBindViewHolder(@NonNull PostCmtAdapter.ViewHolder holder, final int position) {
 
         final Comment item = dataList.get(position);
         holder.userId.setText(item.getLogin_id());
         holder.cmt.setText(item.getText());
+
+        if( date.format(today).equals((item.getC_date()))) {
+            int hour = Integer.parseInt(item.getC_time().substring(0,2));
+            if( hour != rightNow.get(Calendar.HOUR_OF_DAY) ){
+                holder.date.setText((rightNow.get(Calendar.HOUR_OF_DAY) - hour)+"시간 전");
+            }
+            else {
+                int min = Integer.parseInt(item.getC_time().substring(3,5));
+                if( min == rightNow.get(Calendar.MINUTE) )
+                    holder.date.setText("방금 게시됨");
+                else
+                    holder.date.setText((rightNow.get(Calendar.MINUTE) - min) + "분 전");
+            }
+        }
+        else
+            holder.date.setText( item.getC_date()); //게시 날짜
+
+        Glide.with(context).load(RetrofitClient.getBaseUrl() + item.getImg_profile() ).into(holder.profile); // 게시물 사진
+
 
         holder.userId.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,23 +113,44 @@ public class PostCmtAdapter extends BaseAdapter {
                 context.startActivity(intent);
             }
         });
+        holder.profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, ProfileActivity.class);
+                // 게시글 사용자 id 전달
+                intent.putExtra("userId",item.getLogin_id());
+                context.startActivity(intent);
+            }
+        });
 
-        return convertView;
-    }
-    @Override public long getItemId(int position) {
-        return dataList.get(position).getId();
+
+
     }
 
-    @Override public Object getItem(int position) {
-        return dataList.get(position);
-    }
-
-    static class ViewHolder{
+        static class ViewHolder extends   RecyclerView.ViewHolder{
         TextView userId;
         TextView cmt;
-        public ViewHolder(View convertView) {
-            userId = (TextView) convertView.findViewById(R.id.post_item_id);
-            cmt = (TextView) convertView.findViewById(R.id.post_item_cmt);
+        ImageView profile;
+        TextView date;
+
+        public ViewHolder(@NonNull final View itemView, final OnItemLongClickEventListener a_itemLongClickListener) {
+            super(itemView);
+            userId = (TextView) itemView.findViewById(R.id.post_cmt_item_id);
+            cmt = (TextView) itemView.findViewById(R.id.post_item_cmt);
+            profile = itemView.findViewById(R.id.post_item_cmt_pic);
+            date = itemView.findViewById(R.id.post_cmt_item_date);
+
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    final int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        a_itemLongClickListener.onItemLongClick(itemView, position);
+                    }
+                    return false;
+                }
+            });
         }
     }
 
